93ab74a77b2a83d87dd01d8240848e2fea4b9d5e
Danny Nguyen <dannynvn@gmail.com>
2023-05-03 22:52:35 -0700

    build: finished up user and thought models

--- models/Thought.js
+++ models/Thought.js
@@ -2,22 +2,45 @@ const { Schema, model } = require('mongoose');
 const moment = require('moment');
 const reactionSchema = require('./Reaction');
 
-const thoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 280
+const thoughtSchema = new Schema(
+    {
+        // thoughtText is a string, it's required, and has a max length of 280 characters
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        // thought is timestamped when created
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        // username is a string and is required for a thought to be created
+        username: {
+            typ: String,
+            required: true
+        },
+        reactions: [{reactionSchema}]
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        // Use a getter method to format the timestamp on query
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
-    },
-    username: {
-        typ: String,
-        required: true
-    },
-    reactions: [{reactionSchema}]
+    {
+        toJSON: {
+            // use virtuals and getters to format the data
+            virtuals: true,
+            getters: true
+        },
+        // use id instead of _id
+        id: false
+    }
+);
+
+// get total count of reactions on retrieval
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
 });
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,28 +1,47 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-    // username is a string, is required, and must be unique between the collection
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
+const UserSchema = new Schema(
+    {
+        // username is a string, is required, and must be unique between the collection
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        // email is a string, is required, must match a valid email address, and must be unique between the collection
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/]
+        },
+        // thoughts are associated with the user's _id, and they're an array of _id values referencing the Thought model
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        // friends are associated with the user's _id, and they're an array of _id values referencing the User model    
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
     },
-    // email is a string, is required, must match a valid email address, and must be unique between the collection
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/]
-    },
-    // thoughts are associated with the user's _id, and they're an array of _id values referencing the Thought model
-    thoughts: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Thought'
-    }],
-    // friends are associated with the user's _id, and they're an array of _id values referencing the User model    
-    friends: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
-})
+    {
+        // use toJSON to tell schema to use virtuals
+        toJSON: {
+            virtials: true,
+        },
+        // use id instead of _id
+        id: false
+    }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
