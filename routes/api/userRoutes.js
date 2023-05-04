81ada512f644cbb2ec7ffcb84e11ba5980299317
Danny Nguyen <dannynvn@gmail.com>
2023-05-03 22:39:41 -0700

    build: added thought routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,3 +18,5 @@ router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
