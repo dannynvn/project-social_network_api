1251dca1ae7bfab6148f084ca8406104a5897e0d
Danny Nguyen <dannynvn@gmail.com>
2023-05-03 21:33:15 -0700

    build: started user model schema

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+})
